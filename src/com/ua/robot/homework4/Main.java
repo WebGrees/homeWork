f1ad2c40c02af4944e96c7bafeb4b8b3df82f0c6
Web Grees <WebGrees@gmail.com>
2023-01-21 21:14:26 +0200

    String Operations

--- src/com/ua/robot/homework4/Main.java
+++ src/com/ua/robot/homework4/Main.java
@@ -2,6 +2,15 @@ package com.ua.robot.homework4;
 
 public class Main {
     public static void main(String[] args) {
+        String love = "I love Java";
+        System.out.println(love.toUpperCase() + ", " + love.toLowerCase());
 
+        char firstChar = love.charAt(0);
+        System.out.println("First character is: " + firstChar);
+
+        int length = love.length();
+        int lastIndex = length - 1;
+        char lastChar = love.charAt(lastIndex);
+        System.out.println("Last character is: " + lastChar);
     }
 }
