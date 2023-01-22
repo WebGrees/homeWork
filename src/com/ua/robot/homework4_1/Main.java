3eecaf520ca60a35a1ef8d482ac1cf432addff92
Web Grees <WebGrees@gmail.com>
2023-01-22 18:14:37 +0200

    String Operations (second try to commit)

---
+++ src/com/ua/robot/homework4_1/Main.java
@@ -0,0 +1,17 @@
+package com.ua.robot.homework4_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String love = "I love Java";
+        System.out.println(love.toUpperCase() + ", " + love.toLowerCase());
+
+        char firstChar = love.charAt(0);
+        System.out.println("First character is: " + firstChar);
+
+        int length = love.length();
+        int lastIndex = length - 1;
+        char lastChar = love.charAt(lastIndex);
+        System.out.println("Last character is: " + lastChar);
+    }
+}
+
