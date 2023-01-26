f753ebc079f533238d4a689fdf5ed79aa539aa84
Web Grees <WebGrees@gmail.com>
2023-01-26 19:07:05 +0200

    If Statements

--- src/com/ua/robot/homework5/Main.java
+++ src/com/ua/robot/homework5/Main.java
@@ -2,8 +2,8 @@ package com.ua.robot.homework5;
 
 public class Main {
     public static void main(String[] args) {
-        int number1 = 30;
-        int number2 = 30;
+        int number1 = -100;
+        int number2 = 800;
 
         if (number1 > number2) {
             System.out.println(number1 + " is grater than " + number2);
@@ -27,7 +27,8 @@ public class Main {
             System.out.println(number1 + " is even");
         } else {
             System.out.println(number1 + " is odd");
-        }if (number1 == number2) {
+        }
+        if (number1 == number2) {
             System.out.println("");
         } else if (number2 %2 == 0) {
             System.out.println(number2 + " is even");
