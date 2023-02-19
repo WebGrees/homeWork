037acffde173d116c05733e312d6b59397f3701c
Web Grees <WebGrees@gmail.com>
2023-02-19 10:21:50 +0100

    OOP1

---
+++ src/com/ua/robot/homework10/Array.java
@@ -0,0 +1,47 @@
+package com.ua.robot.homework10;
+
+import java.util.Random;
+
+public class Array {
+    public int[] getRandomArray(int length, int minValue, int maxValue) {
+        int[] array = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(minValue, maxValue);
+        }
+
+        return array;
+    }
+
+    public void sortArrayByAscending(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void sortArrayByDescending(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
