fbb7fcb76362a50651bf00095106d499c4d24160
Web Grees <WebGrees@gmail.com>
2023-03-18 22:35:16 +0100

    Enum

---
+++ src/com/ua/robot/homework17/Main.java
@@ -0,0 +1,8 @@
+package com.ua.robot.homework17;
+
+public class Main {
+    public static void main(String[] args) {
+        DayOfTheWeek dayNumber = DayOfTheWeek.SUNDAY;
+        dayNumber.printDayByNumber(2);
+    }
+}
